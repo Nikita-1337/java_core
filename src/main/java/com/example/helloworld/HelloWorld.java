17ab3f36832f6e89d68b2ec286d32280752a57c1
Nikita1337 <tabaze97@gmail.com>
2024-11-11 12:02:02 +0300

    My 2 commit

--- src/main/java/com/example/helloworld/HelloWorld.java
+++ src/main/java/com/example/helloworld/HelloWorld.java
@@ -3,5 +3,23 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("CHAO POKA");
+        int myFirstInt = 1337;
+        short myFirstShort = 3266;
+        long myFirstLong = 1000000;
+        double myFirstDouble = 22.22;
+        float  myFirstFloat = 22.23f;
+        char с = 'U';
+        boolean u = true;
+        byte myFirstByte = 127; // изначально я выставил тут значение в 128, но джава выдала ошибку. Видимо byte подразуемвает до 128
+        String s = "Привет,";
+        String space = " ";
+        String name = "Никита!";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+" "+"guys");
+        System.out.println("My number is " + myFirstInt);
+        String g = "Hello";
+        String a = "guys";
+        System.out.println(myFirstInt);
+        System.out.println("https://i.ytimg.com/vi/R9d0KelayMc/maxresdefault.jpg"); // просьба перейти по ссылке 😁
     }
 }
