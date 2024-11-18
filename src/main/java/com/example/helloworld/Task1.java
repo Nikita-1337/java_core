e9b2451dd93038c7195c2416a41ebc0d6b7133de
Nikita1337 <tabaze97@gmail.com>
2024-11-18 20:07:52 +0300

    не ну мы справились

--- src/main/java/com/example/helloworld/Task1.java
+++ src/main/java/com/example/helloworld/Task1.java
@@ -1,10 +1,17 @@
 package com.example.helloworld;
 
 public class Task1 {
-    public static void main(String [] args){
-        int numberN = 0;
-        if(numberN<1) {
+    public static void main(String[] args) {
+        int numberN = 5;
+        if (numberN < 1) {
             System.out.println("Числа для вывода отсутствуют");
+
+        } else {
+            int i = 1;
+            while (i <= numberN) {
+                System.out.println(i);
+                i++;
+            }
         }
     }
 }
--- src/main/java/com/example/helloworld/Task2.java
+++ src/main/java/com/example/helloworld/Task2.java
@@ -4,15 +4,21 @@ public class Task2 {
     public static void main(String[] args) {
         int sum = 0; // Переменная для хранения суммы чётных чисел
         int count = 0; // Переменная для подсчёта количества чётных чисел
+        int nch= 0;
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
-                sum += i;
+                sum = sum + i;
                 count++;
 
             }
+            // else {
+               // nch++;
+
+          //  }
         }
         System.out.println("Сумма всех чётных чисел от 1 до 100: " + sum);
         System.out.println("Количество чётных чисел от 1 до 100: " + count);
         System.out.println("Толмач: file:///C:/Users/user/Desktop/123456.jpg");
+        //System.out.println("здесь что-то будет"+nch);
     }
 }
